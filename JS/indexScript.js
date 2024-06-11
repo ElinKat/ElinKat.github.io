60179255c26b752b3706927264e5faaf1269c1b7
Elin <elin.katriin.luik@gmail.com>
2024-06-11 19:26:28 +0300

    c

--- JS/indexScript.js
+++ JS/indexScript.js
@@ -1,8 +1,8 @@
 import { sessionTokenCheck, sessionExpirationCheck } from "./JS/session.js";
 import { login } from "./JS/login.js";
-import { addListeners } from "./JS/listeners.js";
+import { loginHandler } from "./JS/listeners.js";
 
 sessionTokenCheck();
-addListeners();
+loginHandler();
 sessionExpirationCheck();
 login();
--- JS/listeners.js
+++ JS/listeners.js
@@ -1,17 +1,17 @@
-import { logout as performLogout } from "./JS/logout.js";
+// import { logout as performLogout } from "./JS/logout.js";
 import { login } from "./JS/login.js";
 
 export function loginHandler() {
 	const loginButton = document.getElementById("login");
 	loginButton.addEventListener("click", () => login());
-	window.addEventListener("keydown", (e) => {
-		if (e.key == "Enter") {
-			login();
-		}
-	});
+	// window.addEventListener("keydown", (e) => {
+	// 	if (e.key == "Enter") {
+	// 		login();
+	// 	}
+	//});
 }
 
-export function logoutHandler() {
-	const logoutButton = document.getElementById("logout");
-	logoutButton.addEventListener("click", () => performLogout());
-}
+// export function logoutHandler() {
+// 	const logoutButton = document.getElementById("logout");
+// 	logoutButton.addEventListener("click", () => performLogout());
+// }
