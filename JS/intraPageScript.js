1b2d449ea83f6c56ee8574c61c7810a01cc502a2
Elin <elin.katriin.luik@gmail.com>
2024-07-02 14:07:03 +0300

    c

--- JS/intraPageScript.js
+++ JS/intraPageScript.js
@@ -13,6 +13,7 @@ import { audits } from "./tablesAndGraphs/audits.js";
 import { placeProgress } from "./tablesAndGraphs/progressChart.js";
 import { fetchGraphInfo } from "./getInfo/graphInfo.js";
 import { displayUserInfo } from "./getInfo/userInfo.js";
+import { fetchUser } from "./getInfo/userInfo.js";
 
 
 userData();
